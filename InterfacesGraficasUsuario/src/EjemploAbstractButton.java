9c4d869f86970b54702bddb6dbdd3cce92540f3b
Monica Blanco <114340444+Monica-74@users.noreply.github.com>
2025-01-27 20:30:52 +0100

    Add files via upload

---
+++ InterfacesGraficasUsuario/src/EjemploAbstractButton.java
@@ -0,0 +1,23 @@
+//import javax.swing.*;
+//
+//public class EjemploAbstractButton extends JFrame {
+//    protected JButton boton = new JButton("Boton");
+//    protected JCheckBox casilla= new JCheckBox("MArcada");
+//    protected
+//    protected
+//    protected
+//
+//    protected
+//    protected
+
+
+
+
+
+
+//
+//
+//    public static void main(String[] args) {
+//
+//    }
+//}
